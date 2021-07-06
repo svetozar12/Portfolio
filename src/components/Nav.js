564496a887bdbc94254e7b1e4abfd64521af21c7
svetozar12 <zaauna@abv.bg>
2021-07-06 21:57:50 +0100

    fixed the issue with the css (btw i am testing new style for writing css mobile first it seems easier so far)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,10 +3,30 @@ import React, { useState } from "react";
 function Nav() {
   const [toggle, setToggle] = useState(false);
 
-  return (<p>some text</p>
-  {
-    toggle ? <p>some true text</p> : <p>some false text</p>;
-  }
+  return (
+    <>
+      <div
+        className={toggle ? "hamburger change" : "hamburger"}
+        onClick={() => setToggle(!toggle)}
+      >
+        <div className="burger-1"></div>
+        <div className="burger-2"></div>
+        <div className="burger-3"></div>
+      </div>
+      <div className={toggle ? "mobile-nav" : "transition"}>
+        <div className="links">
+          {toggle ? (
+            <>
+              <a href="#">Home</a>
+              <a href="#">About</a>
+              <a href="#">Services</a>
+              <a href="#">Skills</a>
+              <a href="#">Projects</a>
+            </>
+          ) : null}
+        </div>
+      </div>
+    </>
   );
 }
 
