ca297b270e68c73632b542b563466f8cd63e4c88
svetozar12 <zaauna@abv.bg>
2021-08-26 23:02:32 +0100

    added particle effect canvas

--- src/components/Header/ParticleBG.js
+++ src/components/Header/ParticleBG.js
@@ -4,26 +4,32 @@ import Particles from "react-particles-js";
 
 function ParticleBG() {
   return (
-    <Particles
-      params={{
-        particles: {
-          number: {
-            value: 50,
-          },
-          size: {
-            value: 3,
-          },
-        },
-        interactivity: {
-          events: {
-            onhover: {
-              enable: true,
-              mode: "repulse",
+    <div className="App">
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 40,
+              density: {
+                enable: true,
+                value_area: 1000,
+              },
+            },
+            stroke: {
+              width: 5,
+              color: "red",
             },
           },
-        },
-      }}
-    />
+        }}
+      />
+      <div className="header-background-image" id="home">
+        <div className="svetozar-gospodinov">
+          <h4>Hello, my name is</h4>
+          <h1>Svetozar Gospodinov</h1>
+          <h2>And i'm a front end developer</h2>
+        </div>
+      </div>
+    </div>
   );
 }
 
